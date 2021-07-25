20db59275921db343be5b031d968c67e267e186b
Rohit Prasan Mandal <rohitmandal814566@gmail.com>
2021-07-25 21:17:15 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -34,7 +34,7 @@ export default function Home() {
             href="https://github.com/vercel/next.js/tree/master/examples"
             className={styles.card}
           >
-            <h3🍀 Examples &rarr;</h3>
+            <h3>🍀 Examples &rarr;</h3>
             <p>A day with cognoPy : Mood Refresher & Usage Statistics</p>
           </a>
 
