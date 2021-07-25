3d456425b3a2dc482490b0ea3e712d77a7547980
Rohit Prasan Mandal <rohitmandal814566@gmail.com>
2021-07-25 15:17:33 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://cognopy.vercel.app">cognoPy!</a>
+          Welcome to <a href="https://github.com/xiaowuc2/cognopy">cognoPy!</a>
         </h1>
 
         <p className={styles.description}>
