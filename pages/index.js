371f74450096967ed6fa46e2b55dbbceea46f49b
Rohit Prasan Mandal <rohitmandal814566@gmail.com>
2021-07-25 08:19:59 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,13 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>cognoPy</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">cognopy!</a>
+          Welcome to <a href="https://cognopy.vercel.app">cognoPy!</a>
         </h1>
 
         <p className={styles.description}>
