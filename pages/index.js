3efd4d2eee515030546ad12a63df924f626b07f0
Rohit Prasan Mandal <rohitmandal814566@gmail.com>
2021-07-27 23:24:35 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -53,7 +53,7 @@ export default function Home() {
            <code className={styles.code}>What's new</code>
          </p>
          <dl>
-           <dt>2.2 {' '}<cdoe className={styles.cdde>27-06-21</code></dt>
+           <dt>2.2 <cdoe className={styles.cdde>27-06-21</code></dt>
            <dd>• Improved file management</dd>
            <dt>2.1</dt>
            <dd>• OCR to detect add metadata from screenshot</dd>
