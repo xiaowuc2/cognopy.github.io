39802cb912083220a7f623f743588190312e4644
Rohit Prasan Mandal <rohitmandal814566@gmail.com>
2021-07-28 00:34:22 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -57,9 +57,9 @@ export default function Home() {
            <dt>2.2 <code>28-06-21</code></dt>
            <dd><img src="https://raw.githubusercontent.com/xiaowuc2/xiaowuc2/master/source/cogno/blue.png" width="7px;"/> Improved file management</dd>
            <dt>2.1</dt>
-           <dd><img src="https://raw.githubusercontent.com/xiaowuc2/xiaowuc2/master/source/cogno/blue.png" width="7px;"/> OCR to detect add metadata from screenshot</dd>
-           <dd><img src="https://raw.githubusercontent.com/xiaowuc2/xiaowuc2/master/source/cogno/blue.png" width="7px;"/> Average mood detection</dd>
-           <dd><img src="https://raw.githubusercontent.com/xiaowuc2/xiaowuc2/master/source/cogno/blue.png" width="7px;"/> Improved memory usage</dd>
+           <dd><img src="https://raw.githubusercontent.com/xiaowuc2/xiaowuc2/master/source/cogno/tel.png" width="7px;"/> OCR to detect add metadata from screenshot</dd>
+           <dd><img src="https://raw.githubusercontent.com/xiaowuc2/xiaowuc2/master/source/cogno/tel.png" width="7px;"/> Average mood detection</dd>
+           <dd><img src="https://raw.githubusercontent.com/xiaowuc2/xiaowuc2/master/source/cogno/tel.png" width="7px;"/> Improved memory usage</dd>
          </dl>
       </main>
 
