d10635e1bf7f0acb35133dcd4e331a293a15fd3c
Rohit Prasan Mandal <rohitmandal814566@gmail.com>
2021-07-28 08:41:22 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -27,7 +27,7 @@ export default function Home() {
 
           <a href="https://nextjs.org/learn" className={styles.card}>
             <h3>Install &rarr;</h3>
-            <p>Installation guideline on GitHub.<code>git clone https://github.com/cognoPy/cognoPy</code></p>
+            <p>Installation guideline.<code>git clone https://github.com/cognoPy/cognoPy</code></p>
           </a>
 
           <a
