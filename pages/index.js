5735a348a3e3f81cdb930583ee30f76064707065
Rohit Prasan Mandal <rohitmandal814566@gmail.com>
2021-08-20 10:00:21 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -48,16 +48,6 @@ export default function Home() {
             </p>
           </a>
         </div>  
-         <p>
-          {' '}
-          <code className={styles.code}>What's new</code>
-           <img src="https://raw.githubusercontent.com/xiaowuc2/xiaowuc2/master/source/cogno/green.png" width="8px;"/>
-         </p>
-         <dl>
-           <dd><img src="https://raw.githubusercontent.com/xiaowuc2/xiaowuc2/master/source/cogno/tel.png" width="7px;"/>  OCR to detect add metadata from screenshot</dd>
-           <dd><img src="https://raw.githubusercontent.com/xiaowuc2/xiaowuc2/master/source/cogno/tel.png" width="7px;"/>  Average mood detection</dd>
-           <dd><img src="https://raw.githubusercontent.com/xiaowuc2/xiaowuc2/master/source/cogno/tel.png" width="7px;"/>  Improved memory usage</dd>
-         </dl>
       </main>
 
       <footer className={styles.footer}>
